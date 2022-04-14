df7951b7f575ed185755974e2ec65c1379bf6654
Aryodjat1 <aryodjati5@gmail.com>
2022-04-14 21:57:53 +0700

    Add Soal Soal No 6

--- src/Mungkinkah.java
+++ src/Mungkinkah.java
@@ -1,35 +1,61 @@
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Mungkinkah {
 
-    public void proses() {
+    public void apakahMungkin() {
+        int k, n, hasil=0;
+
+        ArrayList<Integer> angka = new ArrayList<Integer>();
+
         Scanner scan = new Scanner(System.in);
 
-        int data, k, n;
-
-
-        System.out.println("Nilai k : ");
+        System.out.println("Masukkan nilai k : ");
         k = scan.nextInt();
 
-        System.out.println("Jumlah Angka yang ingin anda masukkan : ");
-        data = scan.nextInt();
+        System.out.println("Masukkan jumlah nilai dalam array : ");
+        n = scan.nextInt();
 
-        System.out.println("--------------------------------------------");
-
-        Integer[] angka = new Integer[data];
-
-        for (int i = 0; i < angka.length; i++) {
-            System.out.println("Masukkan Angka ke-" + (i + 1) + " : ");
-            angka[i] = scan.nextInt();
+        for (int i = 0; i < n; i++){
+            System.out.println("Masukkan data ke-"+(i+1)+" : ");
+            angka.add(scan.nextInt());
         }
-    }
 
-    public void apakahMungkin(){
+        for (int a = 0; a < n; a++){
+            for (int b = 0; b < n; b++){
+                if (angka.get(a) != angka.get(b)){
+                    if (angka.get(a) + angka.get(b) == k){
+                        hasil++;
+                    }
+                }
+            }
+        }
+
+        System.out.print("Angka Yang Anda Masukkan = [");
+
+        for (int c = 0; c < angka.size(); c++){
+            if (c == angka.size()-1){
+                System.out.print(angka.get(c));
+            }else{
+                System.out.print(angka.get(c)+", ");
+            }
+        }
+        System.out.println("]");
+
+        System.out.println("Apakah Mungkin ? ");
+        if (hasil > 0){
+            System.out.println("Mungkin");
+        }else {
+            System.out.println("Tidak Mungkin");
+        }
 
     }
 
     public static void main(String[] args) {
+        Mungkinkah mungkin = new Mungkinkah();
 
+        mungkin.apakahMungkin();
     }
 }
 
